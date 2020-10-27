ad40c6aec7b96d46b9815684bd5778b0475ca9f8
magnustroest <mmt@hellogreatworks.com>
2020-10-27 10:33:07 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+<template>
+    <body>
+        <h1>Insert infromations:</h1>
+        <form action="/action_page.php">
+            <label for="fname">First name:</label>
+            <input type="text" id="fname" name="fname"/><br/>
+            <label for="lname">Last name:</label>
+            <input type="text" id="lname" name="lname"/><br/>
+            <label for="lname">Email:</label>
+            <input type="text" id="email" name="email"/><br/>
+            <input type="submit" value="Submit"/>
+        </form>
+
+    </body>
+</template>
+
