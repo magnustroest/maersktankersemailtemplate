55730a3cef0b74c111367848565a74e8a10aecf1
magnustroest <mmt@hellogreatworks.com>
2020-10-29 09:41:00 +0100

    Api version

--- functions/app.js
+++ functions/app.js
@@ -1,51 +1,42 @@
 const nodemailer = require('nodemailer');
-const express = require('express')
-const bodyParser = require('body-parser')
-const exphbs = require('express-handlebars')
-const path = require('path')
-const serverless = require("serverless-http");
 
-
-const app = express();
-
-const router = express.Router();
-
-//app.engine('handlebars', exphbs());
-//app.set('view engine', 'handlebars');
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use(`/.netlify/functions/app`, router);
-
-router.post('/send', (req, res) => {
-  console.log(req.body)
+exports.handler = async function (event, context) {
+  try {
+    const data = JSON.parse(event.body)
+    console.log(data.email)  
     let transporter = nodemailer.createTransport({
-        host: "smtp.mailtrap.io",
+      host: "smtp.mailtrap.io",
       port: 2525,
-      secure: false, 
+      secure: false,
       auth: {
-          user: '7d62218594b137', 
-          pass: 'b1e3396000d31d'  
+        user: '7d62218594b137',
+        pass: 'b1e3396000d31d'
       },
-      tls:{
-        rejectUnauthorized:false
+      tls: {
+        rejectUnauthorized: false
       }
     });
     let mailOptions = {
-        from: 'yes@test.dk', 
-        to: req.body.email, 
-        subject: 'Node Contact Request', 
-        text: 'Hello world?',
-        html: output 
+      from: 'yes@test.dk',
+      to: data.email,
+      subject: 'Node Contact Request',
+      text: 'Hello world?',
     };
     transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return console.log(error);
-        }
-        console.log('Message sent: %s', info.messageId);   
-        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-  
-        res.render('contact', {msg:'Email has been sent'});
+      if (error) {
+        return console.log(error);
+      }
+      console.log('Message sent: %s', info.messageId);
+      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+      return
     });
-    });
-module.exports = app;
-module.exports.handler = serverless(app);
+  } catch (e) {
+    console.log(e)
+  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Goood" })
+  };
+
+}
